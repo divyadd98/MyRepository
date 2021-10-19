895cc4e5591d866d019717c62af6211685df4a66
divyadd98 <dd19421998@gmail.com>
2021-10-20 02:37:54 +0530

    Second commit

--- src/main/java/com/task1crud/controller/Controller.java
+++ src/main/java/com/task1crud/controller/Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.task1crud.model.Employee;
 import com.task1crud.model.User;
 
-@CrossOrigin(origins = "http://localhost:57736")
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping({ "/employees" })
