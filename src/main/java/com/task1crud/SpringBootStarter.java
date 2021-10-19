166984abe104a0eb191a3bb2fdfd9760167cb0f0
divyadd98 <dd19421998@gmail.com>
2021-10-20 01:16:00 +0530

    First commit

---
+++ src/main/java/com/task1crud/SpringBootStarter.java
@@ -0,0 +1,19 @@
+package com.task1crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarter.class, args);
+	}
+
+	
+}
+
+
+
+
---
+++ src/main/java/com/task1crud/controller/Controller.java
@@ -0,0 +1,77 @@
+package com.task1crud.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.task1crud.model.Employee;
+import com.task1crud.model.User;
+
+@CrossOrigin(origins = "http://localhost:57736")
+
+@RestController
+@RequestMapping({ "/employees" })
+public class Controller {
+
+	private List<Employee> employees = createList();
+
+	@GetMapping(produces = "application/json")
+    public List<Employee> firstPage() {
+		return employees;
+	}
+	
+	@GetMapping(produces = "application/json")
+	@RequestMapping({ "/validateLogin" })
+	public User validateLogin() {
+		return new User("User successfully authenticated");
+	}
+	
+	@DeleteMapping(path = { "/{id}" })
+	public Employee delete(@PathVariable("id") int id) {
+		Employee deletedEmp = null;
+		for (Employee emp : employees) {
+			if (emp.getEmpId().equals(id)) {
+				employees.remove(emp);
+				deletedEmp = emp;
+				break;
+			}
+		}
+		return deletedEmp;
+	}
+
+	@PostMapping
+	public Employee create(@RequestBody Employee user) {
+		employees.add(user);
+		System.out.println(employees);
+		return user;
+	}
+
+	
+	private static List<Employee> createList() {
+		List<Employee> tempEmployees = new ArrayList<>();
+		Employee emp1 = new Employee();
+		emp1.setName("Vidit");
+		emp1.setDesignation("Tester");
+		emp1.setEmpId("1");
+		emp1.setSalary(40000);
+
+		Employee emp2 = new Employee();
+		emp2.setName("Anish");
+		emp2.setDesignation("developer");
+		emp2.setEmpId("2");
+		emp2.setSalary(30000);
+		tempEmployees.add(emp1);
+		tempEmployees.add(emp2);
+		return tempEmployees;
+	}
+
+}
